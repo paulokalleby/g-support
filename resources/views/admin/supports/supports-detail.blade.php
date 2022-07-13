f1202797260cc6d9d522ff7105052c858ce26ddb
Paulo Kallleby <paulo.devweb@gmail.com>
2022-07-13 13:07:50 -0300

    OK

--- resources/views/admin/supports/supports-detail.blade.php
+++ resources/views/admin/supports/supports-detail.blade.php
@@ -39,7 +39,7 @@
                                         <h5 class="mb-1">{{ $attendance }}</h5>
                                     </div>
                                 </div>
-                                <small>{{ dateTimeToBr($updated) }}</small>
+                                {{--<small>{{ dateTimeToBr($updated) }}</small>--}}
                             </div>
                             @if ($solution)
                                 <p class="text-sm mb-0">{{ $solution }}</p>
