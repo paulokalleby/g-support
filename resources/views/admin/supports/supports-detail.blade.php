7e7450a0395e1b6aae4002bbd7de50866942243e
Paulo Kallleby <paulo.devweb@gmail.com>
2022-07-15 13:37:29 -0300

    OK

--- resources/views/admin/supports/supports-detail.blade.php
+++ resources/views/admin/supports/supports-detail.blade.php
@@ -95,7 +95,7 @@
 
                             @if ($status == 'attending' && $attendance_id == auth()->user()->id)
                                 <button type="button" wire:loading.attr="disabled" wire:click.prevent="finishing"
-                                    class="btn btn-primary btn-sm ml-auto">
+                                    class="btn btn-primary btn-sm ml-auto mb-3">
                                     Finalizar
                                     <i wire:loading wire:target="finishing" class="fas fa-spinner fa-pulse"></i>
                                 </button>
